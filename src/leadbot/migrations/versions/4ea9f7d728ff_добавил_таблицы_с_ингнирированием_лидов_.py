c262611cd2043d9bd2f26618247979cdde13471d
DIMFLIX-OFFICIAL <dimflix.official@gmail.com>
2025-02-11 11:53:55 +0300

    Восстановил версии

--- src/leadbot/migrations/versions/4ea9f7d728ff_добавил_таблицы_с_ингнирированием_лидов_.py
+++ src/leadbot/migrations/versions/4ea9f7d728ff_добавил_таблицы_с_ингнирированием_лидов_.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '4ea9f7d728ff'
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = "90301f6074b6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
